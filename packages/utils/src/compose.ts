8019d7928db7931bac820b0b3b103fee21b0bf31
Fred Souza <1396615+fmsouza@users.noreply.github.com>
2026-05-23 22:16:48 +0100

    feat(utils): add pipe + flow [utils-03]
    Add typed pipe and flow function composition helpers (up to arity 6).

---
+++ packages/utils/src/compose.ts
@@ -0,0 +1,18 @@
+export function pipe<A>(a: A): A
+export function pipe<A, B>(a: A, ab: (a: A) => B): B
+export function pipe<A, B, C>(a: A, ab: (a: A) => B, bc: (b: B) => C): C
+export function pipe<A, B, C, D>(a: A, ab: (a: A) => B, bc: (b: B) => C, cd: (c: C) => D): D
+export function pipe<A, B, C, D, E>(a: A, ab: (a: A) => B, bc: (b: B) => C, cd: (c: C) => D, de: (d: D) => E): E
+export function pipe<A, B, C, D, E, F>(a: A, ab: (a: A) => B, bc: (b: B) => C, cd: (c: C) => D, de: (d: D) => E, ef: (e: E) => F): F
+export function pipe(a: unknown, ...fns: ReadonlyArray<(x: unknown) => unknown>): unknown {
+  return fns.reduce((acc, fn) => fn(acc), a)
+}
+
+export function flow<A, B>(ab: (a: A) => B): (a: A) => B
+export function flow<A, B, C>(ab: (a: A) => B, bc: (b: B) => C): (a: A) => C
+export function flow<A, B, C, D>(ab: (a: A) => B, bc: (b: B) => C, cd: (c: C) => D): (a: A) => D
+export function flow<A, B, C, D, E>(ab: (a: A) => B, bc: (b: B) => C, cd: (c: C) => D, de: (d: D) => E): (a: A) => E
+export function flow<A, B, C, D, E, F>(ab: (a: A) => B, bc: (b: B) => C, cd: (c: C) => D, de: (d: D) => E, ef: (e: E) => F): (a: A) => F
+export function flow(...fns: ReadonlyArray<(x: unknown) => unknown>): (a: unknown) => unknown {
+  return (a) => fns.reduce((acc, fn) => fn(acc), a)
+}
